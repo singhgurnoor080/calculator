2ad8851addfce44ce7d4b141d5c99b603fd64d62
hirschi675 <logan.loganhirschi@Alveyworld-06.local>
2013-12-04 10:50:41 -0700

    team 3 functions

---
+++ team3.py
@@ -0,0 +1,14 @@
+# Logan Hirschi, Jon Page, Coolidge B, Deydrien F, Josh S.
+
+def pow(b, e):
+	""" This function will raise the number b to the power e """
+	total = 1
+	for i in range(e):
+		total = total * b
+	
+	return total
+	
+	
+def ln(x):
+	""" This function will give the natural log  """
+	return 1
