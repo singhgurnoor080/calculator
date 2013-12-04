43fc1ffab569168c0be20ccb92ee4c9425b15570
linkwolfy <cameronculligan@gmail.com>
2013-12-04 10:30:15 -0700

    Added comments

--- team2.py
+++ team2.py
@@ -1,12 +1,13 @@
 #Team2 Cameron Culligan, Ryan Barlow, Dakota McElroy, Joseph Drake, Xylonah Stott,
 
 def abs(x):
-	pass
+	""" this function will return the absolute value of a number """
 	if (x) <= 0:
 		return (x)
 	
 	
 def factorial(n):
+	""" Returns the factorial of a number """
     if n == 0:
         return 1
     else:
