e38a69b6bbc7a429016ef27cca32a899db762d8e
linkwolfy <cameronculligan@gmail.com>
2013-12-04 10:20:46 -0700

    fixed Absolute Value, Added Factorial function

--- team2.py
+++ team2.py
@@ -1,7 +1,15 @@
-#Team2 Cameron Culliga, Ryan Barlow, Dakota McElroy, Joseph Drake, Xylonah Stott,
+#Team2 Cameron Culligan, Ryan Barlow, Dakota McElroy, Joseph Drake, Xylonah Stott,
 
 def abs(x):
 	pass
-	if (x) == <0
-	return: (x)
+	if (x) <= 0:
+		return (x)
+	
+	
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 	
